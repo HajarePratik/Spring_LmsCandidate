6cd9f83c38a6900570693812f5ac2de6055601b5
HajarePratik <pratikhajare09@gmail.com>
2021-08-21 10:23:05 +0530

    [Pratik]Add : API is Running on Eureka Server

--- src/main/java/com/bridgelabz/lmscandidate/SpringBootLmsCandidateApplication.java
+++ src/main/java/com/bridgelabz/lmscandidate/SpringBootLmsCandidateApplication.java
@@ -2,12 +2,15 @@ package com.bridgelabz.lmscandidate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan("com.bridgelabz.lmscandidate")
 @EnableJpaRepositories("com.bridgelabz.lmscandidate.respository")
+@EnableEurekaClient
+
 public class SpringBootLmsCandidateApplication 
 {
 	public static void main(String[] args) {
